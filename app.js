f0ffd30a79d1d03ea8d51c5789143b293dc12e0f
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-02-19 14:40:34 -0800

    added api info

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 // install express and create app
 const express = require ('express');
-const data = require ('./geo.js');
 const app = express();
+require('dotenv').config();
 const request = require ('superagent');
 const weather = require ('./darksky.js');
 const cors = require ('cors');
 
 app.use(cors());
+app.use((err, req, res, next) => {
+    req.rosalie = 'its me, its working';
+    next();
+});
 
 let lat;
 let lng;
 
 // create location route that reads from query params, gets some dummy data, and returns json
-app.get('/location', (request, respond) => {
-    const cityData = data.results[0];
+app.get('/location', async(req, respond, next) => {
+    try {
+        // ins www.cool-api.com?search=portland, `location` will be portland
+        const location = req.query.search;
+        // TODO: HIDE KEY
+        const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${location}&format=json`;
+        const cityData = await request.get(URL);
+        const firstResult = cityData.body[0];
 
-    lat = cityData.geometry.location.lat;
-    lng = cityData.geometry.location.lng;
+        lat = firstResult.lat;
+        lng = firstResult.lng;
 
-    respond.json({
-        formatted_query: cityData.formatted_address,
-        latitude: cityData.geometry.location.lat,
-        longitude: cityData.geometry.location.lng,
-    });
+        respond.json({
+            formatted_query: firstResult.display_name,
+            latitude: lat,
+            longitude: lng,
+        });
+    } catch (err) {
+        next (err);
+    }
 });
 
 const getWeatherData = (lat, lng) => {
@@ -33,18 +46,22 @@ const getWeatherData = (lat, lng) => {
         };
     });  
 };
-app.get('/weather', (request, respond) => {
+app.get('/weather', (req, res) => {
     // use the lat and lng from earlier to get weather data for the selected area
     const latLng = getWeatherData(lat, lng);
     
-    respond.json(latLng);
+    res.json(latLng);
 });
+
+app.get('*', (req, res) =>
+    res.send('404'));
+
+module.exports = {
+    app: app,
+};
 
 const port = process.env.PORT || 4001;
 app.listen(port, () => {
     console.log('listening on port', port);
 });
 
-// app.get('*', (req, res) =>
-//     res.send('404')
-// }
